5846c323cdf56c3794745378ffdd7f6f0197162e
Kouame <kouamedjah@example.com>
2022-05-02 10:13:15 -0500

    message

--- myAp/for-directive/src/app/slides/slides.component.ts
+++ myAp/for-directive/src/app/slides/slides.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Planet } from '../Planet';
 
 @Component({
   selector: 'app-slides',
@@ -7,10 +12,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidesComponent implements OnInit {
   slideContainer: any;
+  planet: Planet | any;
+  name: string | any;
 
-  constructor() { }
+  planets: any ={};
+  
+
+
+  constructor(private formBuilder : FormBuilder,private http: HttpClient, private router:Router) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  getaPlanet():Observable<any>{
+    return this.http.get<any>("http://localhost:9090/planet/");
+  }
+  getPlanet():Observable<any>{
+    return this.http.get<any>("http://localhost:9090/planet/");
+  }
 
   ngOnInit(): void {
   }
-  
+
+  planetInfo(tt: any){
+    alert(tt);
+   // this.getaPlanet(this.name).subscribe(data=>{this.planet=data;});
+  }
+
+  home(){
+    this.router.navigate(['home'])
+  }
+
+  btn:boolean=false;
+btnClick(name: any){
+  alert("Picture Clicked");
+  this.btn =! this.btn;
+ // this.getPlanet().subscribe(data=>{this.planet=data;});
+ this.http.get<any>("http://localhost:9090/planet/"+name)
+ .subscribe(data=>{this.planet=data;});
 }
+
+plt: string | any;
+
+mercury(){
+  this.plt = "Mercury";
+  this.btnClick("Mercury");
+  
+
+}
+
+earth(){
+  this.plt = "Earth";
+  this.btnClick("Earth");
+}
+
+venus(){
+  this.plt = "Venus";
+  this.btnClick(this.plt);
+}
+
+mars(){
+  this.plt = "Mars";
+  this.btnClick(this.plt);
+}
+
+jupiter(){
+  this.plt = "Jupiter";
+  this.btnClick(this.plt);
+}
+
+saturn(){
+  this.plt = "Saturn";
+  this.btnClick(this.plt);
+}
+
+uranus(){
+  this.plt = "Uranus";
+  this.btnClick(this.plt);
+}
+
+neptune(){
+  this.plt = "Neptune";
+  this.btnClick(this.plt);
+}
+}
+
+
